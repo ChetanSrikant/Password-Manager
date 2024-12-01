c90528f12b16ec04e21e6fafa7ca49c82f46a687
ChetanSrikant <chetansmandiga@gmail.com>
2024-12-01 22:43:08 +0530

    toggle password complete

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import eye from "../assets/eye.svg";
+import closed from "../assets/eyeclosed.svg"
+import { useRef } from "react";
 
 const Manager = () => {
+    const ref = useRef()
+    const showPassword = () => {
+        alert("show the password");
+        if(ref.current.src === closed){
+            ref.current.src = eye;
+        }else{
+            ref.current.src = closed;
+        }
+    };
+    
   return (
     <>
-      <div class="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div>
       </div>
 
       <div className="mycontainer">
@@ -16,20 +29,28 @@ const Manager = () => {
         <p className="text-green-900 text-lg text-center">
           Your own Password Manager
         </p>
-        <div className="text-white flex flex-col p-4 text-black gap-8 items-center">
+        <div className=" flex flex-col p-4 gap-8 items-center">
           <input
+          placeholder="Enter Website URL"
             className="rounded-full border border-green-500 w-full p-4 py-1"
             type="text"
           />
           <div className="flex w-full gap-8 ">
             <input
+            placeholder="Enter Username"
               className="rounded-full border border-green-500 w-full p-4 py-1"
               type="text"
             />
+            <div className="relative">                
             <input
-              className="rounded-full border border-green-500 w-full p-4 py-1"
-              type="text"
+            placeholder="Enter Password"
+            className="rounded-full border border-green-500 w-full p-4 py-1"
+            type="text"
             />
+            <span className="absolute right-0 cursor-pointer" onClick={showPassword}>
+                <img ref={ref} className="p-1.5" src={eye} alt="eye" />
+            </span>
+            </div>
           </div>
           <button className="flex justify-center items-center gap-2 text-black rounded-full bg-green-500 border border-green-900 px-8 py-2 w-fit
           hover:bg-green-400">
