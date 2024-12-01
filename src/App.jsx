98b296724d8c4d30a6bd8cc167e146932b37ad6a
ChetanSrikant <chetansmandiga@gmail.com>
2024-12-01 18:47:31 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Navbar from './Components/Navbar'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    <Navbar></Navbar>
       MY UI
     </>
   )
