114bac3140b338727c9c4fea4223cf3499448906
ChetanSrikant <chetansmandiga@gmail.com>
2024-12-01 20:42:32 +0530

    Manager UI and fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './Components/Navbar';
+import Manager from './Components/Manager';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Navbar></Navbar>
-      MY UI
+      <Navbar />
+      <Manager />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Manager = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div class="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div>
+      </div>
 
-export default Manager
+      <div className="mycontainer">
+        <h1 className="text-4xl font-bold text-center">
+          <span className="text-green-500">&lt;</span>
+          Pass
+          <span className="text-green-500">OP/ &gt;</span>
+        </h1>
+        <p className="text-green-900 text-lg text-center">
+          Your own Password Manager
+        </p>
+        <div className="text-white flex flex-col p-4 text-black gap-8 items-center">
+          <input
+            className="rounded-full border border-green-500 w-full p-4 py-1"
+            type="text"
+          />
+          <div className="flex w-full gap-8 ">
+            <input
+              className="rounded-full border border-green-500 w-full p-4 py-1"
+              type="text"
+            />
+            <input
+              className="rounded-full border border-green-500 w-full p-4 py-1"
+              type="text"
+            />
+          </div>
+          <button className="flex justify-center items-center gap-2 text-black rounded-full bg-green-500 border border-green-900 px-8 py-2 w-fit
+          hover:bg-green-400">
+          <lord-icon
+            src="https://cdn.lordicon.com/jgnvfzqg.json"
+            trigger="hover"
+          ></lord-icon>
+          Add Password</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Manager;
